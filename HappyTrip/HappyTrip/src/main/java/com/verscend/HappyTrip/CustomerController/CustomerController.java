67e2d700dd40d460f9d0aa06f3244c63882d7c90
Nitish Manandhar <i11132@d2hs.com>
2018-08-27 16:23:46 +0545

    Third commit

--- HappyTrip/HappyTrip/src/main/java/com/verscend/HappyTrip/CustomerController/CustomerController.java
+++ HappyTrip/HappyTrip/src/main/java/com/verscend/HappyTrip/CustomerController/CustomerController.java
@@ -1,15 +1,14 @@
 package com.verscend.HappyTrip.CustomerController;
 
-import org.json.JSONObject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.ModelAttribute;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -29,28 +28,24 @@ public class CustomerController {
 	public List<Customers> getAll(){
 		return (List<Customers>) cusRep.findAll();
 	}
-	@ResponseBody
-	@RequestMapping(value = "/add",method = RequestMethod.POST)
-	public void insert(@RequestBody String json) {
-		Customers customer = new Customers();
-		
-		//creating json object form given stringified json data
-		JSONObject JsonData =new JSONObject(json);
-		
-		//setting the data from form to the customer object
-		customer.setFirstName( JsonData.getString("firstName"));
-		customer.setLastName(JsonData.getString("lastName"));
-		customer.setAge(JsonData.getInt("age"));
-		customer.setEmail(JsonData.getString("email"));
-		customer.setPassword(JsonData.getString("password"));
-		customer.setGender(JsonData.getString("gender"));
-		customer.setPhoneNo( JsonData.getString("phoneNo"));
-		System.out.println(customer.toString());
-		
-		//saving the data to the database
-		cusRep.save(customer);
 	
+	@RequestMapping(value = "/login")
+	public void login()
+	{
+		
 	}
+	@RequestMapping(value = "/add",method = RequestMethod.POST)
+	public void insert(@RequestBody Customers customer) {
+		//saving the data to the database
+		System.out.println(customer.toString());
+		cusRep.save(customer);
+		
+	}
+	@RequestMapping(value = "/update")
+	public void update() {
+		System.out.println("data updated");
+	}
+	
 	@SuppressWarnings("deprecation")
 	@Bean
 	public WebMvcConfigurer corsConfigurer() 
